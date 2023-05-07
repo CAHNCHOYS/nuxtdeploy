7fec16ee1051c3348ad8b05601f322649effe6ff
CAHNCHOYS <lullullullul94@gamil.com>
2023-05-07 18:58:06 +0300

    try 3

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
   <p>
     <v-btn variant="flat" color="pink" @click="makeCall">Make Call</v-btn>
     <v-btn variant="flat" color="pink" @click="makeCall2">Make Call2</v-btn>
+    <v-btn variant="flat" color="pink" @click="makeCall3">Make Call3</v-btn>
   </p>
 </template>
 
@@ -13,7 +14,6 @@ const makeCall = async () => {
       method: "POST",
       body: { id: 20 },
     });
-
   } catch (error) {
     console.log(error);
   }
@@ -22,16 +22,24 @@ const makeCall2 = async () => {
   try {
     const data = await $fetch("/api/pic", {
       method: "POST",
-      body: JSON.stringify({id: "alex"}),
+      body: JSON.stringify({ id: "alex" }),
     });
     console.log(data, "call2");
-
   } catch (error) {
     console.log(error);
   }
 };
 
-
+const makeCall3 = async () => {
+  try {
+    const data = await $fetch("/api/getData", {
+      method: "GET",
+    });
+    console.log(data, "call2");
+  } catch (error) {
+    console.log(error);
+  }
+};
 </script>
 
 <style scoped></style>
---
+++ server/api/getData.get.ts
@@ -0,0 +1,10 @@
+export default eventHandler(async (event) => {
+
+  
+   console.log("method called");
+  
+
+
+    return "Data from getData get Method";
+  });
+  
